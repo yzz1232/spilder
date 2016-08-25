4f7b27c29563669af1197c5a7fe9b4d901fe530d
zhuzhenyu1994 <875930090@qq.com>
2016-08-25 17:25:02 +0800

    知乎

---
+++ src/main/java/demo/splider/ZhihuPageProcessor.java
@@ -0,0 +1,64 @@
+package demo.splider;
+
+import java.util.List;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.pipeline.ConsolePipeline;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+public class ZhihuPageProcessor implements PageProcessor{
+
+	private Site site = Site.me()
+						/**
+						.setUserAgent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0")
+						//.setDomain("zhuhu.com")
+						.addCookie(".zhuhu.com", "login", "M2M3ZDJlZGYwNTU0NDc1OWEwNzdiOTY0MjQ5ZDZlZGQ=|1472094707|84efba10952b839fd9a2c7feda9fc1804f87018e")
+						.addCookie(".zhuhu.com", "q_c1","6d6d8f4d6fdb4118a520c395e381567a|1472094693000|1472094693000")
+						.addCookie(".zhuhu.com", "z_c0","Mi4wQUFBQW44Y2lBQUFBRUVCelNQOXZDaGNBQUFCaEFsVk44LTdsVndDYV9haTAxWUlRYWJldzhZWmduUlVENHVTNWx3|1472094751|13f65c13b95fd7128ef5652b5930939fc05407c8")
+						.addCookie("_xsrf","f36076fb095ebb5fcb8daf090112864a")
+						.addCookie("_za","7e676b71-0b27-4f68-85b2-4e558faa4887")
+						.addCookie(".zhuhu.com", "a_t", "2.0AAAAn8ciAAAAEEBzSP9vChcAAABhAlVN8-7lVwCa_ai01YIQabew8YZgnRUD4uS5lw")
+						.addCookie(".zhuhu.com", "d_c0","AHAAJisAcAqPTgAQtaB-DNj_GmwUl5eN21o=|1472094928")
+						.addCookie(".zhuhu.com", "_zap","3e07dd5f-ae84-4e4c-b4da-0dd00908c761")
+						.addCookie(".zhuhu.com","__utma","51854390.1051483914.1472094995.1472094995.1472094995.1")
+						.addCookie(".zhuhu.com","__utmb","51854390.4.10.1472094995")
+						.addCookie(".zhuhu.com","__utmc","51854390")
+						.addCookie(".zhuhu.com", "__utmz","51854390.1472094995.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)")
+						.addCookie(".zhuhu.com","__utmv","51854390.100-1|2=registration_date=20131222=1^3=entry_date=20131222=1")
+						.addCookie(".zhuhu.com","__utmt", "1")
+						*/
+						.setRetryTimes(3).setSleepTime(1000);
+	
+	public Site getSite() {
+		
+		return site;
+	}
+	
+	public void process(Page page) {
+		//List<String> list = page.getHtml().xpath("//a[@class='question_link']").all();
+		List<String> list = page.getHtml().links().regex("(https://www.zhihu.com/question/\\d+)").all();
+		page.addTargetRequests(list);
+		page.putField("title", page.getHtml().xpath("//span[@class='zm-editable-content']/text()").toString());
+		List<String> contentList =  page.getHtml().xpath("//div[@class='zm-editable-content clearfix']").all();
+		List<String> authorList = page.getHtml().xpath("//a[@class='author-link']/text()").all();
+		List<String> countList = page.getHtml().xpath("//span[@class='count']/text()").all();
+		if(page.getResultItems().get("title")==null){
+			page.setSkip(true);
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		Spider
+		.create(new ZhihuPageProcessor())
+		.addUrl("https://www.zhihu.com/topic/19550447/top-answers")
+		.addPipeline(new ConsolePipeline())
+		.thread(5)
+		.run();
+	}
+
+	
+	
+}
